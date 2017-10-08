926209ff34feb0a855b25e22a29436e726d8934f
Christian <neocsr@gmail.com>
2017-10-08 00:28:16 -0700

    Update rosbag image extractor and notebook

--- rosbag_image_extractor.py
+++ rosbag_image_extractor.py
@@ -3,15 +3,15 @@ import rosbag
 from cv_bridge import CvBridge, CvBridgeError
 
 
-bag_name = './traffic_light_bag_files/just_traffic_light.bag'
-out_dir = './just_traffic_light/'
+bag_name = './traffic_light_bag_files/loop_with_traffic_light.bag'
+out_dir = './loop_with_traffic_light/'
 
 bridge = CvBridge()
 
 with rosbag.Bag(bag_name, 'r') as bag:
     for topic, msg, ts in bag.read_messages():
         if topic == '/image_raw':
-            img_out_name = out_dir + str(ts.secs) + str(ts.nsecs) + '.png'
+            img_out_name = '%s%d-%09d' % (out_dir, ts.secs, ts.nsecs) + '.png'
             image = bridge.imgmsg_to_cv2(msg, 'bgr8')
             out = cv2.imwrite(img_out_name, image)
             if not out:
